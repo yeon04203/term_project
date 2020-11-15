2f1e094183c905692d93da0a45fd4c39734ce24f
clappingmin <clappingmin@naver.com>
2020-11-15 14:09:53 +0900

    chatbot

--- app/views.py
+++ app/views.py
@@ -26,44 +26,6 @@ def index(request):
     context = {"todays": todays}
     return render(request, 'index.html', context)
 
-
-# #----------------------------------
-# # 챗봇
-# chatbot = ChatBot(
-#     'Ron Obvious',
-#     trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
-
-# # Train based on the english corpus
-
-# #Already trained and it's supposed to be persistent
-# # chatbot.train("chatterbot.corpus.english")
-
-# @csrf_exempt
-# def get_response(request):
-# 	response = {'status': None}
-
-# 	if request.method == 'POST':
-# 		data = json.loads(request.body.decode('utf-8'))
-# 		message = data['message']
-
-# 		chat_response = chatbot.get_response(message).text
-# 		response['message'] = {'text': chat_response, 'user': False, 'chat_bot': True}
-# 		response['status'] = 'ok'
-
-# 	else:
-# 		response['error'] = 'no post data found'
-
-# 	return HttpResponse(
-# 		json.dumps(response),
-# 			content_type="application/json"
-# 		)
-
-# def chatbot(request, template_name="chatbot.html"):
-#     context = {'title': 'Chatbot Version 1.0'}
-#     return render(None, template_name, context)
-
-
-# # --------------------------------------------------------
 def voca(request):
     vocas = Voca.objects.all()
     paginator = Paginator(vocas, 10)
